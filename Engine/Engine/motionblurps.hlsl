98c99cf5f70bf0fd85ab51c88f09d0c94a5d8923
Mhanbai <gregormanby@gmail.com>
2018-04-20 03:06:41 +0100

    Managed now to have 2 different textures in a shader. Need to find some way to get a texture that is a map of depth

--- Engine/Engine/motionblurps.hlsl
+++ Engine/Engine/motionblurps.hlsl
@@ -6,7 +6,7 @@
 /////////////
 // GLOBALS //
 /////////////
-Texture2D shaderTexture;
+Texture2D shaderTexture[2];
 SamplerState SampleType;
 
 
@@ -29,12 +29,12 @@ float4 MotionBlurPixelShader(PixelInputType input) : SV_TARGET
     int numSamples = 3;
     float2 texCoord = input.tex;
 
-	float4 color = shaderTexture.Sample(SampleType, texCoord);
+	float4 color = shaderTexture[0].Sample(SampleType, texCoord);
     texCoord += input.velocity;
 
     for (int i = 0; i < numSamples; i++, texCoord += input.velocity)
     {
-        float4 newColor = shaderTexture.Sample(SampleType, texCoord);
+        float4 newColor = shaderTexture[0].Sample(SampleType, texCoord);
 		color += newColor;
     }
 	
